d0328477371aed6cba0ce638def33e9c314d581b
yoshimatsu567 <febbasyoungmason@gmail.com>
2022-05-15 01:55:54 +0900

    update: resas_apiの型修正

--- src/utils/resas_api/index.ts
+++ src/utils/resas_api/index.ts
@@ -1,12 +1,17 @@
 import axios from 'axios';
 
-import { PrefectureType, ResasErrorType } from '@/types';
+import { PopulationDataType, PrefectureType, ResasErrorType } from '@/types';
 
 import { NEXT_PUBLIC_API_KEY } from '@/utils/constants/index';
 
-export const fetchPrefectureList = async () => {
+type FetchPrefectureListType = () => Promise<{
+    data: PrefectureType[];
+    error: ResasErrorType | undefined;
+}>;
+
+export const fetchPrefectureList: FetchPrefectureListType = async () => {
     let data: PrefectureType[] = [];
-    let error: ResasErrorType = undefined;
+    let error: ResasErrorType | undefined = undefined;
 
     await axios
         .get('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
@@ -25,25 +30,31 @@ export const fetchPrefectureList = async () => {
     return { data, error };
 };
 
-export const fetchPopulationDataByPrefCode = async (prefCode: number) => {
-    let data!: { year: number; value: number }[];
-    let error: { name: string; message: string } | undefined = undefined;
+type FetchPopulationDataByPrefCodeType = (prefCode: number) => Promise<{
+    data: PopulationDataType;
+    error: ResasErrorType | undefined;
+}>;
 
-    await axios
-        .get(
-            `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?&prefCode=${prefCode}`,
-            {
-                headers: { 'X-API-KEY': NEXT_PUBLIC_API_KEY() },
-            }
-        )
-        .then((res) => {
-            data = res.data.result?.data[0]?.data;
-            const statusCode = res.data.statusCode;
-            if (!data && statusCode !== 200) {
-                // responseに都道府県のデータがない && statusCodeが200でない場合をErrorと判定する
-                error = { name: statusCode, message: res.data.message };
-            }
-        });
+export const fetchPopulationDataByPrefCode: FetchPopulationDataByPrefCodeType =
+    async (prefCode: number) => {
+        let data: PopulationDataType = [];
+        let error: ResasErrorType | undefined = undefined;
 
-    return { data, error };
-};
+        await axios
+            .get(
+                `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?&prefCode=${prefCode}`,
+                {
+                    headers: { 'X-API-KEY': NEXT_PUBLIC_API_KEY() },
+                }
+            )
+            .then((res) => {
+                data = res.data.result?.data[0]?.data;
+                const statusCode = res.data.statusCode;
+                if (!data && statusCode !== 200) {
+                    // responseに都道府県のデータがない && statusCodeが200でない場合をErrorと判定する
+                    error = { name: statusCode, message: res.data.message };
+                }
+            });
+
+        return { data, error };
+    };
