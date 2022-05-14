1f806058770e9d94e5c9004c2464e5de537a46d9
yoshimatsu567 <febbasyoungmason@gmail.com>
2022-05-15 03:07:35 +0900

    update: 都道府県データをgetStaticProps()で取得

--- src/hooks/usePrefectureList.tsx
+++ src/hooks/usePrefectureList.tsx
@@ -1,46 +1,18 @@
-import { useCallback, useEffect } from 'react';
+import { SetterOrUpdater, useSetRecoilState } from 'recoil';
 
-import { SetterOrUpdater, useRecoilState, useSetRecoilState } from 'recoil';
-import { useErrorHandler } from 'react-error-boundary';
-
-import { isLoadingState } from '@/states/atoms/asyncStatusAtom';
-import { prefectureListState } from '@/states/atoms/prefectureListAtom';
-
-import { fetchPrefectureList } from '@/utils/resas_api';
+import { PrefectureCheckBoxType, PrefectureType } from '@/types';
 
 import { checkedPrefectureState } from '@/states/atoms/checkedPrefectureAtom';
-import { PrefectureType, PrefectureCheckBoxType } from '@/types';
+import { prefectureListState } from '@/states/atoms/prefectureListAtom';
 
 export type UsePrefectureListType = () => {
-    prefectureList: PrefectureType[];
+    setPrefectureList: SetterOrUpdater<PrefectureType[]>;
     setCheckedPrefecture: SetterOrUpdater<PrefectureCheckBoxType | null>;
 };
 
 const usePrefectureList: UsePrefectureListType = () => ({
-    // チェックボックス用の都道府県のデータ
-    prefectureList: useFetchPrefectureList(),
+    setPrefectureList: useSetRecoilState(prefectureListState),
     setCheckedPrefecture: useSetRecoilState(checkedPrefectureState),
 });
 
 export default usePrefectureList;
-
-export const useFetchPrefectureList = () => {
-    const [prefectureListData, setPrefectureListData] =
-        useRecoilState(prefectureListState);
-    const setIsLoading = useSetRecoilState(isLoadingState);
-    const handleError = useErrorHandler();
-
-    useEffect(() => {
-        if (!prefectureListData.length) {
-            fetchPrefectureList().then((res) => {
-                if (res.error) {
-                    handleError(res.error);
-                }
-                setPrefectureListData(res.data);
-                setIsLoading(false);
-            });
-        }
-    }, []);
-
-    return prefectureListData;
-};
