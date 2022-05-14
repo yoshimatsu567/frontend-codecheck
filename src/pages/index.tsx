6d2cc54947b19b370a55ab957648011965f78769
yoshimatsu567 <febbasyoungmason@gmail.com>
2022-05-14 18:46:05 +0900

    update: HomeでHeaderを使用

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
 import type { NextPage } from 'next';
+
+// components
 import Head from 'next/head';
+import { Header } from '@/components/molecules/common/Header';
 
 const Home: NextPage = () => {
     return (
-    <>
-        <Head>
-            <title>県別の総人口推移表示するアプリ</title>
-            <meta name='description' content='県別の総人口推移表示するアプリ' />
-            <link rel='icon' href='/favicon.png' />
-        </Head>
-        Home Page
-    </>
-    ) ;
+        <>
+            <Head>
+                <title>県別の総人口推移表示するアプリ</title>
+                <meta
+                    name='description'
+                    content='県別の総人口推移表示するアプリ'
+                />
+                <link rel='icon' href='/favicon.png' />
+            </Head>
+            <Header />
+        </>
+    );
 };
 
 export default Home;
