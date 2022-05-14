e0c004557a91892c26f04ec8fe3229c68e00783c
yoshimatsu567 <febbasyoungmason@gmail.com>
2022-05-14 23:18:52 +0900

    add: fetchPopulationDataByPrefCode()の追加

--- src/utils/resas_api/index.ts
+++ src/utils/resas_api/index.ts
@@ -24,3 +24,25 @@ export const fetchPrefectureList = async () => {
 
     return { data, error };
 };
+
+export const fetchPopulationDataByPrefCode = async (prefCode: number) => {
+    let data!: { year: number; value: number }[];
+    let error: { name: string; message: string } | undefined = undefined;
+
+    await axios
+        .get(
+            `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?&prefCode=${prefCode}`,
+            {
+                headers: { 'X-API-KEY': NEXT_PUBLIC_API_KEY() },
+            }
+        )
+        .then((res) => {
+            data = res.data.result?.data[0]?.data;
+            const statusCode = res.data.statusCode;
+            if (!data && statusCode !== 200) {
+                error = { name: statusCode, message: res.data.message };
+            }
+        });
+
+    return { data, error };
+};
