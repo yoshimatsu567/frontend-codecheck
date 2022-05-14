6851e10403a7e712b6c551888575f693d7cd18a7
yoshimatsu567 <febbasyoungmason@gmail.com>
2022-05-14 17:37:00 +0900

    update: Headを設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
-    return <div>Home Page</div>;
+    return (
+    <>
+        <Head>
+            <title>県別の総人口推移表示するアプリ</title>
+            <meta name='description' content='県別の総人口推移表示するアプリ' />
+            <link rel='icon' href='/favicon.png' />
+        </Head>
+        Home Page
+    </>
+    ) ;
 };
 
 export default Home;
